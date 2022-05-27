adc5e40ca2f78094b3e9624b64cc65fdc0dea8b4
Mitsukai74 <ansaanva@gmail.com>
2022-05-27 18:45:06 -0500

    se incluye imagenes y funcionalidad de palabras

--- scripting.js
+++ scripting.js
@@ -5,19 +5,33 @@ String.prototype.replaceAt=function(index, character) {
 const palabras=['aleta','leon','nintendo','traje','paleta'];
 const palabraEscogida=palabras[Math.floor(Math.random()*palabras.length)];
 let palabraguionada=palabraEscogida.replace(/./g,"_ ");
-//alert(palabraEscogida + " - " + palabraguionada);
+let contador=0;
 document.querySelector('#salida').innerHTML=palabraguionada;
 document.querySelector('#verificar').addEventListener('click',() =>
 {
     const letra = document.querySelector('#letra').value;
+    let haFallado=true;
      
     for (const i in palabraEscogida){
         if (letra==palabraEscogida[i]){
             palabraguionada=palabraguionada.replaceAt(i*2,letra);
-            
+            haFallado=false;
+        }
+    }
+    if (haFallado){
+        contador++;
+        document.querySelector('#imgAhorcado').style.backgroundPosition = -(230*contador)+ 'px 0';
+        if (contador==7){
+            alert("Has perdido");
+        }
+    }else{
+        if(palabraguionada.indexOf('_')){
+            alert("Has adivinado");
         }
     }
     document.querySelector('#salida').innerHTML=palabraguionada;
+    document.querySelector('#letra').value = "";
+    document.querySelector('#letra').focus();
 })
     
     
